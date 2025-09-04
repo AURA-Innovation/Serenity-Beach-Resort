74ca211928ab4094c11cc8081428a6f5bafba74e
AURA-Innovation <infinitzinc@gmail.com>
2025-09-04 06:18:59 -0600

    [dyad] Fix useVapi error by wrapping the app in VapiProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { VapiProvider } from "@/components/vapi/VapiProvider";
+import { ENV } from "@/environment";
 
 const queryClient = new QueryClient();
 
@@ -13,13 +15,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <VapiProvider apiKey={ENV.YOUR_PUBLIC_API_KEY} assistantId={ENV.YOUR_ASSISTANT_ID}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </VapiProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
